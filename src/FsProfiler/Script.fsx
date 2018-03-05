c29f241a66f7d1ec5e64dbc219252098b9cb89a8
Christian Steinert <christiansteinert@kpmg.com>
2018-03-05 15:32:30 +0100

    Adds prototypical implementation of a sink and formatter to the script.

--- src/FsProfiler/Script.fsx
+++ src/FsProfiler/Script.fsx
@@ -22,17 +22,18 @@ let WP () =
     WrappingProfiler.Profile "Test" impl
 
 
-DP ()     
-
-
-WP ()
-
-
 type TraceEvent =
-| TaskStart of Guid * string
-| SubtaskStart of Guid * parent : Guid * string
-| TaskStop of Guid * int64
-| SubtaskStop of Guid * parent : Guid * int64
+| TaskStart of Guid * Guid option * string
+| TaskStop of Guid * Guid option * int64
+
+type Task =
+    {
+        Id : Guid
+        Name : string
+        SubTasks : Task list
+        DurationInMilliseconds : int64
+    }
+
 
 type TracePrinter () as this =
     inherit EventListener ()
@@ -42,21 +43,50 @@ type TracePrinter () as this =
 
     let queue = new System.Collections.Concurrent.ConcurrentQueue<TraceEvent> ()
 
-    member __.Dump () =
-        queue
-        |> Seq.iter (fun c -> printfn "%A" c)
+    member __.GetTasks () = 
+        let tasks = queue |> Seq.toList
+        let rec mapTask parent c =
+            match c with
+            | TaskStart (id, p, name) when p = parent -> 
+                let timing = tasks |> List.pick (fun c -> 
+                    match c with 
+                    | TaskStop (cid, p, time) when id = cid && p = parent -> time |> Some
+                    | _ -> None)
+                { 
+                    Id = id
+                    Name = name
+                    SubTasks = tasks |> List.choose (mapTask (id |> Some))
+                    DurationInMilliseconds = timing 
+                } |> Some
+            | _ -> None
+
+        tasks
+        |> List.map (mapTask None)
+        |> List.choose id
 
     override __.OnEventWritten args =
         if args.EventSource.Name = "FsProfilerEvents" then
             match args.EventId with
-            | 1 -> TaskStart(args.Payload.[1] :?> Guid, args.Payload.[0] :?> string)
-            | 2 -> SubtaskStart(args.Payload.[2] :?> Guid, args.Payload.[3] :?> Guid, args.Payload.[0] :?> string)
-            | 3 -> TaskStop(args.Payload.[2] :?> Guid, args.Payload.[1] :?> int64)
-            | 4 -> SubtaskStop(args.Payload.[3] :?> Guid, args.Payload.[4] :?> Guid, args.Payload.[2] :?> int64)
+            | 1 -> TaskStart(args.Payload.[1] :?> Guid, None, args.Payload.[0] :?> string)
+            | 2 -> TaskStart(args.Payload.[2] :?> Guid, args.Payload.[3] :?> Guid |> Some, args.Payload.[0] :?> string)
+            | 3 -> TaskStop(args.Payload.[2] :?> Guid, None, args.Payload.[1] :?> int64)
+            | 4 -> TaskStop(args.Payload.[3] :?> Guid, args.Payload.[4] :?> Guid |> Some, args.Payload.[2] :?> int64)
             | _ -> failwith "Unkown event"
 
             |> queue.Enqueue
 
 
 let tp = new TracePrinter ()
-tp.Dump ()
+
+DP ()
+DP ()
+WP ()
+
+let printTasks tasks = 
+    let rec printTask level task =
+        printfn "%*s%s" (level * 4) "" task.Name
+        task.SubTasks |> List.iter (printTask (level + 1))
+        printfn "%*s---Done in %ims" (level * 4) "" task.DurationInMilliseconds
+    tasks |> List.iter (printTask 0)
+    
+tp.GetTasks () |> printTasks
