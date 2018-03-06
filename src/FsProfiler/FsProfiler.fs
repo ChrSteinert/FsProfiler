27f5f8a4be591e8ae134ab2a028e0c3ab35c87ec
Christian Steinert <christiansteinert@kpmg.com>
2018-03-06 15:59:30 +0100

    Supresses console prints for ETW events in RELEASE builds.

--- src/FsProfiler/FsProfiler.fs
+++ src/FsProfiler/FsProfiler.fs
@@ -14,22 +14,30 @@ type FsProfilerEvents private () =
 
     [<Event(1, Message = "Task {0} started.", Level = EventLevel.LogAlways)>]
     member this.TaskStart(taskName : string, taskId : Guid) =
+        #if DEBUG
         printfn "Task %s started." taskName
+        #endif
         this.WriteEvent(1, taskName, taskId)
 
     [<Event(2, Message = "Subtask {0} of task {1} started.", Level = EventLevel.LogAlways)>]
     member this.SubtaskStart(taskName : string, parentTaskName : string, taskId : Guid, parentTaskId : Guid) =
+        #if DEBUG
         printfn "Subtask %s of task %s started." taskName parentTaskName
+        #endif
         this.WriteEvent(2, taskName, parentTaskName, taskId, parentTaskId)
 
     [<Event(3, Message = "Task {0} took {1} ms.", Level = EventLevel.Informational)>]
     member this.TaskStop(taskName : string, milliseconds : int64, taskId : Guid) =
+        #if DEBUG
         printfn "Task %s took %i ms." taskName milliseconds
+        #endif
         this.WriteEvent(3, taskName, milliseconds, taskId)
 
     [<Event(4, Message = "Subtask {0} of task {1} took {2} ms.", Level = EventLevel.Informational)>]
     member this.SubtaskStop(taskName : string, parentTaskName : string, milliseconds : int64, taskId : Guid, parentTaskId : Guid) =
+        #if DEBUG
         printfn "Subtask %s of task %s took %i ms." taskName parentTaskName milliseconds
+        #endif
         this.WriteEvent(4, taskName, parentTaskName, milliseconds, taskId, parentTaskId)
 
     interface IDisposable with
