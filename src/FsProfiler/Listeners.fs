dfb9282c5911005cbb57344c70760a9a102de88a
Christian Steinert <chr.steinert@outlook.com>
2023-11-30 10:47:27 +0100

    Convert to 2 spaces intendation.

--- src/FsProfiler/Listeners.fs
+++ src/FsProfiler/Listeners.fs
@@ -9,65 +9,65 @@ open FsProfiler
 type private RunningTask = Guid option * string
 
 type Task =
-    {
-        Id : Guid
-        Name : string
-        SubTasks : Task list
-        DurationInMilliseconds : int64
-    }
+  {
+    Id : Guid
+    Name : string
+    SubTasks : Task list
+    DurationInMilliseconds : int64
+  }
 
 type ObservableTaskListener () as this =
-    inherit EventListener ()
+  inherit EventListener ()
 
-    do
-        this.EnableEvents(FsProfilerEvents.Log, EventLevel.LogAlways)        
+  do
+    this.EnableEvents(FsProfilerEvents.Log, EventLevel.LogAlways)
 
-    let subscribers = ResizeArray<IObserver<Task>> ()
-    let runningTasks = ConcurrentDictionary<Guid, RunningTask> ()
-    let finishedSubtasks = ConcurrentDictionary<Guid, Task list> ()
+  let subscribers = ResizeArray<IObserver<Task>> ()
+  let runningTasks = ConcurrentDictionary<Guid, RunningTask> ()
+  let finishedSubtasks = ConcurrentDictionary<Guid, Task list> ()
 
-    override __.OnEventWritten args =
-        let getSubtasks key = 
-            match finishedSubtasks.TryGetValue key with
-            | (true, c) -> c |> List.rev
-            | (false, _) -> []
+  override __.OnEventWritten args =
+    let getSubtasks key =
+      match finishedSubtasks.TryGetValue key with
+      | (true, c) -> c |> List.rev
+      | (false, _) -> []
 
-        if args.EventSource.Name = "FsProfilerEvents" then
-            match args.EventId with
-            | 1 -> 
-                let key = args.Payload.[1] :?> Guid
-                runningTasks.AddOrUpdate(key, (None, args.Payload.[0] :?> string), fun _ _ -> failwith "Nope!")
-                |> ignore
-            | 2 -> 
-                let key = args.Payload.[2] :?> Guid
-                runningTasks.AddOrUpdate(key, (args.Payload.[3] :?> Guid |> Some, args.Payload.[0] :?> string), fun _ _ -> failwith "Nope!")
-                |> ignore
-            | 3 -> 
-                let key = args.Payload.[2] :?> Guid
-                let start = runningTasks.[key]
-                let timing = args.Payload.[1] :?> int64
-                { Id = key; Name = start |> snd; DurationInMilliseconds = timing; SubTasks = key |> getSubtasks  }
-                |> (fun c -> 
-                    subscribers |> Seq.iter (fun d -> d.OnNext c))
-            | 4 -> 
-                let key = args.Payload.[3] :?> Guid
-                let parent = args.Payload.[4] :?> Guid
-                let timing = args.Payload.[2] :?> int64
-                let start = runningTasks.[key]
-                let subtask = { Id = key; Name = start |> snd; DurationInMilliseconds = timing; SubTasks = key |> getSubtasks }
-                finishedSubtasks.AddOrUpdate(parent, [ subtask ], fun _ value -> subtask :: value) |> ignore
-            | _ -> ()
+    if args.EventSource.Name = "FsProfilerEvents" then
+      match args.EventId with
+      | 1 ->
+        let key = args.Payload.[1] :?> Guid
+        runningTasks.AddOrUpdate(key, (None, args.Payload.[0] :?> string), fun _ _ -> failwith "Nope!")
+        |> ignore
+      | 2 ->
+        let key = args.Payload.[2] :?> Guid
+        runningTasks.AddOrUpdate(key, (args.Payload.[3] :?> Guid |> Some, args.Payload.[0] :?> string), fun _ _ -> failwith "Nope!")
+        |> ignore
+      | 3 ->
+        let key = args.Payload.[2] :?> Guid
+        let start = runningTasks.[key]
+        let timing = args.Payload.[1] :?> int64
+        { Id = key; Name = start |> snd; DurationInMilliseconds = timing; SubTasks = key |> getSubtasks  }
+        |> (fun c ->
+          subscribers |> Seq.iter (fun d -> d.OnNext c))
+      | 4 ->
+        let key = args.Payload.[3] :?> Guid
+        let parent = args.Payload.[4] :?> Guid
+        let timing = args.Payload.[2] :?> int64
+        let start = runningTasks.[key]
+        let subtask = { Id = key; Name = start |> snd; DurationInMilliseconds = timing; SubTasks = key |> getSubtasks }
+        finishedSubtasks.AddOrUpdate(parent, [ subtask ], fun _ value -> subtask :: value) |> ignore
+      | _ -> ()
 
-    override this.Dispose () =
-        this.DisableEvents(FsProfilerEvents.Log)
-        subscribers.Clear ()
-        base.Dispose ()
+  override this.Dispose () =
+    this.DisableEvents(FsProfilerEvents.Log)
+    subscribers.Clear ()
+    base.Dispose ()
 
-    interface IObservable<Task> with
-        member __.Subscribe observer : IDisposable = 
-            subscribers.Add observer
-            {
-                new IDisposable with
-                    member __.Dispose () = 
-                        subscribers.Remove observer |> ignore
-            }
+  interface IObservable<Task> with
+    member __.Subscribe observer : IDisposable =
+      subscribers.Add observer
+      {
+        new IDisposable with
+          member __.Dispose () =
+            subscribers.Remove observer |> ignore
+      }
--- src/FsProfiler/Output.fs
+++ src/FsProfiler/Output.fs
@@ -4,25 +4,25 @@ open Listeners
 
 module TaskPrinter =
 
-    open System.Text
+  open System.Text
 
-    let sprint task = 
-        let builder = StringBuilder ()
-        let rec printTask level task =
-            Printf.bprintf builder "%*s%s\n" (level * 4) "" task.Name
-            task.SubTasks |> List.iter (printTask (level + 1))
-            Printf.bprintf builder "%*s--- %ims\n" (level * 4) "" task.DurationInMilliseconds
-        task |> printTask 0
-        builder.ToString ()
+  let sprint task =
+    let builder = StringBuilder ()
+    let rec printTask level task =
+      Printf.bprintf builder "%*s%s\n" (level * 4) "" task.Name
+      task.SubTasks |> List.iter (printTask (level + 1))
+      Printf.bprintf builder "%*s--- %ims\n" (level * 4) "" task.DurationInMilliseconds
+    task |> printTask 0
+    builder.ToString ()
 
-    let print task = 
-        task |> sprint |> printf "%s"
+  let print task =
+    task |> sprint |> printf "%s"
 
-    let bprint builder task =
-        task |> sprint |> Printf.bprintf builder "%s"
+  let bprint builder task =
+    task |> sprint |> Printf.bprintf builder "%s"
 
-    let eprint task =
-        task |> sprint |> Printf.eprintf "%s"
+  let eprint task =
+    task |> sprint |> Printf.eprintf "%s"
 
-    let fprint writer task =
-        task |> sprint |> Printf.fprintf writer "%s"
+  let fprint writer task =
+    task |> sprint |> Printf.fprintf writer "%s"
